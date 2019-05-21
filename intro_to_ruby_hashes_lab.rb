61b1c91b981be1d05c1bf158670464dcf0f2d440
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 14:37:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_fourth_tier
 	  railroads: 
 	    {pieces: 4, 
 	    names: 
-	      {reading_railroad: {mortage_value: "$100"}, 
+	      {reading_railroad: {"mortage_value" => "$100"}, 
 	      pennsylvania_railroad: {mortage_value: "$100"}, 
 	      b_and_o_railroad: {mortage_value: "$400"},
 	      shortline: {mortage_value: "$800"}
